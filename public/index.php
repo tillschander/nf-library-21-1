a7e2c4583cdaaacd10e1ecaaf193961756b93630
Till Schander <tillschander@gmail.com>
2021-03-24 14:45:23 +0100

    Request / Response Objects

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 error_reporting(E_ALL);
@@ -8,20 +11,19 @@ $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
-$uri = $_SERVER['REQUEST_URI'];
-$method = $_SERVER['REQUEST_METHOD'];
+$request = Request::createFromGlobals();
+$response = new Response();
 $routes = require __DIR__ . '/../config/routes.php';
-$response = '';
 
 foreach ($routes as $route) {
-    if ($method === $route[0] && $uri === $route[1]) {
-        $response = call_user_func($route[2]);
+    if ($request->getMethod() === $route[0] && $request->getRequestUri() === $route[1]) {
+        $content = call_user_func($route[2]);
+        $response->setContent($content);
     }
 }
 
-if (!$response) {
-    http_response_code(404);
-    $response = '404 - Not Found';
+if (!$response->getContent()) {
+    $response = new Response('404 - Not Found', 404);
 }
 
-echo $response;
+$response->send();
