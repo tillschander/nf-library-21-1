be107a1bb093f57835e59cf2dd37dd13421ffa5e
Till Schander <tillschander@gmail.com>
2021-03-24 12:10:40 +0100

    Added routing

---
+++ config/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    ['GET', '/', function () {
+        return file_get_contents(__DIR__ . '/../templates/index.html');
+    }],
+    ['GET', '/category', function () {
+        return file_get_contents(__DIR__ . '/../templates/category.html');
+    }],
+    ['GET', '/item', function () {
+        return file_get_contents(__DIR__ . '/../templates/item.html');
+    }],
+    ['GET', '/cart', function () {
+        return file_get_contents(__DIR__ . '/../templates/cart.html');
+    }],
+    ['POST', '/cart', function () {
+        return '';
+    }],
+    ['GET', '/cart/success', function () {
+        return file_get_contents(__DIR__ . '/../templates/success.html');
+    }],
+    ['POST', '/borrow', function () {
+        return '';
+    }]
+];
--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "Hello world";
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+$routes = require __DIR__ . '/../config/routes.php';
+$response = '';
+
+foreach ($routes as $route) {
+    if ($method === $route[0] && $uri === $route[1]) {
+        $response = call_user_func($route[2]);
+    }
+}
+
+echo $response;
