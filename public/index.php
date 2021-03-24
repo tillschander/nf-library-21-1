6e96e6179b3dd53fe41d7ab20204d4606115dbc7
Till Schander <tillschander@gmail.com>
2021-03-24 14:07:21 +0100

    Added whoops

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 $routes = require __DIR__ . '/../config/routes.php';
@@ -9,6 +17,11 @@ foreach ($routes as $route) {
     if ($method === $route[0] && $uri === $route[1]) {
         $response = call_user_func($route[2]);
     }
+}
+
+if (!$response) {
+    http_response_code(404);
+    $response = '404 - Not Found';
 }
 
 echo $response;
